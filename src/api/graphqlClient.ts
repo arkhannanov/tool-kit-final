1fb32c94f6a72cbe93f07130b341180ee73e8ecf
Artur Khannanov <arkhannanov@gmail.com>
2024-07-28 05:49:57 +0300

    fix: fix deploy issues

--- src/api/graphqlClient.ts
+++ src/api/graphqlClient.ts
@@ -1,17 +1,18 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import {ApolloClient, InMemoryCache, HttpLink, createHttpLink} from '@apollo/client';
+import {setContext} from "@apollo/client/link/context";
+
 
 const httpLink = createHttpLink({
     uri: 'https://api.github.com/graphql',
 });
 
-const accessToken = process.env.ACCESS_TOKEN;
+console.log(import.meta.env.VITE_ACCESS_TOKEN)
 
 const authLink = setContext((_, { headers }) => {
     return {
         headers: {
             ...headers,
-            authorization: accessToken ? `Bearer ${accessToken}` : '',
+            authorization: import.meta.env.VITE_ACCESS_TOKEN ? `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}` : '',
         },
     };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Export the Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+});
